a6b382224ee2b92f84c724335a5dc53709ac29cd
Zhaobin <zhu.zhaobin@hotmail.de>
2022-11-04 06:24:16 +0100

    new git origin

---
+++ mpi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+Stage0 += baseimage(image='fuchs', _distro='ubuntu22')
+
+gcc_version = USERARG.get('gcc_version', 'gcc@9.3.0 target=x86_64')
+mpi_version = USERARG.get('mpi_version', 'openmpi@4.1.3%gcc@9.3.0 target=x86_64')
+fftw_version = USERARG.get('fftw_version', 'fftw target=IvyBridge')
+Stage0 += shell(commands=
+        [       
+            '. /load-spack-env.sh',
+            'spack env deactivate',
+            'spack install {}'.format(gcc_version),
+            'spack compiler add $(spack location -i {})'.format(gcc_version),
+        ])
+Stage0 += shell(commands=
+        [       
+            '. /load-spack-env.sh',
+            'spack add {}'.format(mpi_version),
+            'spack add {}'.format(fftw_version),
+            'spack install',
+            'spack clean --all'
+        ])
+
+
+
